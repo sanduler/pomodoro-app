cfd4c734551c5ee3ea98cfe47c159360c8d1e546
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-10 09:44:39 -0800

    added the comments

--- main.py
+++ main.py
@@ -22,15 +22,23 @@ LONG_BREAK_MIN = 20
 # TODO: ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # TODO: ---------------------------- UI SETUP ------------------------------- #
+# initilize the tkinter Class
 window = tkinter.Tk()
+# change the title of the screen
 window.title("Pomodoro")
+# set the window size and background
 window.config(padx=140, pady=50, bg=YELLOW)
+# create a new canvas and set the windth, hieght, and change
 canvas = tkinter.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# imported the image file to add to canvas
 tomato_img = tkinter.PhotoImage(file="img/pomodoro.png")
+# adjisted the image on the window
 canvas.create_image(100, 112, image=tomato_img)
+# added the text layered on top of the picture
 canvas.create_text(100, 133, text="00:00", fill="white", font=(FONT_NAME, 36, "bold"))
+# pack the canvas
 canvas.pack()
-
 # center the window to the middle of the screen
 window.eval('tk::PlaceWindow . center')
+# prevent the screen from closing
 window.mainloop()
